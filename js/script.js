f55b91df384b79439be29ba5bf93c9bfd0440a1e
Alex <budnikoleksii@gmail.com>
2020-05-19 15:08:40 +0300

    changed with promises

--- js/script.js
+++ js/script.js
@@ -150,56 +150,73 @@ window.addEventListener('DOMContentLoaded', function() {
     // Form 
     const message = {
         loading: "Загрузка...",
-        success: 'Спасибо! Скоро мы с вами свяжемся',
-        failure: 'Что-то пошло не так...'
+        sucsess: "Спасибо! Скоро мы с Вами свяжемся!",
+        failure: "Что-то пошло не так..."
     };
-
-    const forms = document.querySelectorAll('form'),
-          input = document.getElementsByTagName('input'),
-          statusMessage = document.createElement('div');
-
-    statusMessage.classList.add('status');
-
-    forms.forEach(item => {
-        bindPostData(item);
-    });
-
-    function bindPostData(form) {
-        form.addEventListener('submit', function(event) {
+     
+    const form = document.querySelector('.main-form'),
+        formDown = document.querySelector('#form'),
+        input = form.getElementsByTagName ('input'),
+        statusMessage = document.createElement('div');
+         
+     
+        statusMessage.classList.add('status');
+     
+    function sendForm(form){
+     
+        form.addEventListener('submit', function (event) {
             event.preventDefault();
-
             form.append(statusMessage);
-
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-
+        
             const formData = new FormData(form);
-            // some bug with second form, check it
-            const obj = {};
-            formData.forEach(function(value, key) {
-                obj[key] = value;
+        
+        function postData() {
+            return new Promise(function (resolve, reject) {
+                const request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+                request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        
+                request.onreadystatechange = function () {
+                    if(request.readyState < 4) {
+                        resolve();
+                    } else if (request.readyState === 4) {
+                        if (request.status == 200 && request.status < 300) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    }
+                };
+        
+                let obj = {};
+                formData.forEach (function (value, key) {
+                    obj[key] = value;
+                });
+                const json = JSON.stringify(obj);
+                request.send(json);
+     
+     
             });
-            const json = JSON.stringify(obj);
-            // const json = JSON.stringify(Object.fromEntries(formData.entries()));
-
-            request.send(json);
-
-            request.addEventListener('readystatechange', function() {
-                if (request.readyState < 4) {
-                    statusMessage.innerHTML = message.loading;
-                } else if(request.readyState === 4 && request.status == 200) {
-                    statusMessage.innerHTML = message.success;
-                } else {
-                    statusMessage.innerHTML = message.failure;
-                }
-            });
-
+        }
+        
+        function clearInput() {
             for (let i = 0; i < input.length; i++) {
                 input[i].value = '';
             }
+        }
+        
+        postData (formData)
+            .then (() => statusMessage.innerHTML = message.loading)
+            .then (() => statusMessage.innerHTML = message.sucsess)
+            .catch (() => statusMessage.innerHTML = message.failure)
+            .then (clearInput);
         });
+     
+     
     }
+     
+    sendForm(form);
+    sendForm(formDown);
     // form
 
 });
